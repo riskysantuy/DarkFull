f325a5f99367a003c50dffb6a37a5b30241b7129
RiskySantuy <55824407+riskysantuy@users.noreply.github.com>
2019-09-29 16:20:17 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,34 @@
+clear
+toilet -f standard "Install Tools" -F gay
+
+
+
+
+
+
+clear
+pkg update
+clear
+pkg upgrade
+clear
+pkg install python2
+clear
+pkg install python3
+clear
+pkg install php
+clear
+pkg isntall nano
+clear
+pkg install git
+clear
+pkg isntall ruby
+clear
+pkg install cowsay
+clear
+gem install lolcat
+clear
+pkg install lolcat
+clear
+pip2 install mechanize
+clear
+pip2 isntall requests
