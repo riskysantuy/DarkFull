f409fe10090ad7c27e7e7371728bc184c0617dac
RiskySantuy <55824407+riskysantuy@users.noreply.github.com>
2019-10-01 14:29:17 +0700

    Update Mau-Recode-Sampah-Tak-Ada-Modal-Bangsat-Kau-WKWKWKWKWKKWWkwkwkwkwkwkwkwkwkw.sh

--- Mau-Recode-Sampah-Tak-Ada-Modal-Bangsat-Kau-WKWKWKWKWKKWWkwkwkwkwkwkwkwkwkw.sh
+++ Mau-Recode-Sampah-Tak-Ada-Modal-Bangsat-Kau-WKWKWKWKWKKWWkwkwkwkwkwkwkwkwkw.sh
@@ -21,81 +21,87 @@ endc='\E[0m'
 enda='\033[0m'
 
 clear
-sleep 1
-echo $me"<[∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆]>"
-sleep 1
-echo $bi"<[∆"      $i"Kumpulan Tools Work"
-sleep 1
-echo $pur"<[∆"      $pur"Berbagai Jenis"
-sleep 1
-echo $ku"<[∆"  $ku"Github : riskysantuy(Riski-Santuy)"
-sleep 1
-echo $i"<[∆"   $me"Team   : Cyber Termux Indonesia"
-sleep 1
-echo $bi"<[∆"  $bi"Contact: 0821-6979-67**"
-sleep 1
-echo $me"<[∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆]>"
-sleep 1
-sleep 2
+# yellow= \033[33;1m
+# indigo= \033[34;1m
+# red= \033[31;1m
+# purple= \033[37;1m
+# cyan= \033[36;1m
+# white= \033[39;1m
+z() {
+echo "
+$i░░█░█▌█▀▀█ █▀▀█ █▀▀█ █▌▄█░░
+░░█▀█▌█▄▄█ █▄▄█ █▄▄█ █▄██░░
+░░█░█▌█▌▐█ █▌░░ █▌░░ ░██░░░
+░$ku╔╦═╦╗───╔╗──────╔╗─$me▄▄─▄▄▀▀▄▀▀▄
+░$ku║║║║╠═╦═╣╠╦═╦═╦╦╝║$me███████───▄▀
+ $ku║║║║║╩╣╩╣═╣╩╣║║║╬║$me▀█████▀▀▄▀
+░$ku╚═╩═╩═╩═╩╩╩═╩╩═╩═╝──$me▀█▀ ♥♥
+        $pur◕▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬◕
+             $pu █████████ 
+              █▄█████▄█ 
+              █$me ▼▼▼▼▼$pu █ 
+            $pu ██▌    $pu ██▌ 
+             $pu █$me ▲▲▲▲▲$pu █ 
+              █████████ 
+              ██     ██
+ $i▂▃▅▇█▓▒░۩۞۩$me╔══╗$cy╔═╗$i╔═╗$ku╔╗$bi─╔══╗$pur╔╗╔╗$i۩۞۩░▒▓█▇▅▃▂
+            $me╚╗╔╝$cy║║║$i║║║$ku║║$bi─║══╣$pur╚╗╔╝
+            $me ║║ $cy║║║$i║║║$ku║╚╗$bi╠══║$pur╔╝╚╗
+            $me ╚╝ $cy╚═╝$i╚═╝$ku╚═╝$bi╚══╝$pur╚╝╚╝
+"
+}
+z
 echo
-cowsay -f eyes "WELCOME TO TERMUX" | lolcat
 sleep 1
-toilet -f standard "Tools-Lengkap" -F gay
-sleep 1
-sleep 3
-echo $i"[][][][][][][][][][][][][][][][][][][][][]"
+echo $pur"==================="$cy" ============================="
 sleep 2
-echo $i"[] SILAHKAN MENGGUNAKAN TOOLS WORK 2019 []"
-sleep 1
-echo $i"[] Tools Yang Tersedia Cuma Ada 12 Tools[]"
-sleep 1
-echo $i"[][][][][][][][][][][][][][][][][][][][][]"
-sleep 1
-echo
-echo $pur"CTRL+z Stop" "
+echo $i"Ôûê"$pu"  GitHub    :"$me" riskysantuy"$i"             Ôûê"
 sleep 3
+echo $i"Ôûê"$pu"  FACEBOOK  :"$cy" Hi Tumbal Face'boom Hi"$i"  Ôûê"
+sleep 4
+echo $i"Ôûê"$pu"  WhatsApp  :"$i" *************"$i"            Ôûê"
+sleep 2
+echo $i"Ôûê"$pu"  GMAIL     :"$ku" riskysantuy101@gmail.com"$i"Ôûê"
+sleep 6
+echo $i"Ôûê"$pu"  INSTAGRAM :"$bi" Tidak Ada Soryy"$i"         Ôûê"
+sleep 4
+echo $pur"==================="$cy" ============================="
+echo
 sleep 1
 sleep 4
-echo $cy"============================================"
-echo $bi"1. DarkFB 1.3                       "$i"|ON|"
-echo $cy"============================================"
+echo $ku"MeNu Tools Vvip"
 sleep 1
-echo $me"2. Tuan-Badut                       "$i"|ON|"
-echo $cy"============================================"
-sleep 1
-echo $bi"3. DarkCyber                        "$i"|ON|"
-echo $ku"User:BOCIL Pass:N00B"                
-echo $cy"============================================"
-sleep 1
-echo $me"4. Multi-Bruser-Facebook  (MBF)     "$i"|ON|"
-echo $cy"============================================"
-sleep 1
-echo $bi"5. BAJINGANv6                       "$i"|ON|"     
-echo $bi"User:BAJINGAN Pass:Gans"
-echo $cy"============================================"
-sleep 1
-echo $me"6. Rusmana-ID                       "$i"|ON|"
-echo $pur"Tidak Pake User & Pass"
-echo $cy"============================================"
 sleep 2
-echo $me"7. XtermuxRider                     "$i"|ON|"
-echo $cy"============================================"
-sleep 1
-echo $ku"8. Spam Whatshaap                   "$i"|ON|"
-echo $cy"============================================"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"01"$me"[]"$cy"DarkFB 1.3                []"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"02"$me"[]"$cy" TuanBadut                []"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"03"$me"[]"$cy" DarkCyber                []"
+echo $me"[]"$i"##"$me"[]"$cy" User:BOCIL Pass:N00B     []"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"04"$me"[]"$cy" MBF                      []"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"05"$me"[]"$cy" Bajingan                 []"
+echo $me"[]"$i"##"$me"[]"$cy" User:BAJINGAN Pass:Gans  []"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"06"$me"[]"$cy" Rusmana-ID               []"
+echo $me"[]"$i"##"$me"[]"$cy" Tidak Pake User & Pass   []"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"07"$me"[]"$cy" XtermuxRider             []"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"08"$me"[]"$cy" SPAM Whatshapp           []"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"09"$me"[]"$cy" SPAM SMS                 []"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"10"$me"[]"$cy" Script-Deface-Creator    []"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"11"$me"[]"$cy" Spam Call                []"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"12"$me"[]"$cy" Santet-Online            []"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][]"
 sleep 2
-echo $bi"9. Spam SMS                          "$me"|Maslah|"
-echo $cy"============================================"
 sleep 1
-echo $ku"10.Script Deface Creator            "$i"|ON|"
-echo $cy"============================================"
-sleep 1
-echo $bi"11.Spam Call                        "$i"|ON|"
-echo $cy"============================================"
-sleep 1
-echo $me"12.Santet Online                    "$i"|ON|"
-echo $cy"============================================"
-sleep 2
 echo $pur"["$pur"Masukan Pilihan Anda"$ku"]"
 read -p "Nomor Berapa>>>>>>>" pil
  
@@ -128,15 +134,6 @@ git clone https://github.com/fatonicyber/Darkcyber
 cd Darkcyber
 sh Darkcyber.sh
 fi
-
-
-#Facebok Target
-
-3) clear
-toilet -f standard "Wait" -F gay
-git clone https://github.com/fatonicyber/Darkcyber
-cd Darkcyber
-sh Darkcyber.sh
 
 if [ $pil = 04 ]
 then
@@ -230,24 +227,3 @@ git clone https://github.com/Gameye98/santet-online
 cd santet-online
 python2 santet.py
 fi
-
-if [ $pil = 00 ]
-then
-clear
-figlet -f slant "W A I T"|Ulolcat
-sleep 2
-echo $cy"Terima Kasih sudah mengunakan Tools ini"
-sleep 2
-echo $i"Silahkan di pakai semoga bermanfaat Buat anda"
-sleep 2
-echo $pur"Bila Ada Kesalahan Anda Bisa Nanya Melalui Via"
-sleep 2
-echo $ku"Facebook :"$i" Hi Tumbal Face'boom HI"
-echo $ku"WhatsApp :"$i" ***********"
-sleep 2
-echo $pur"Terima kasih Yang Sudah Support dengan Tools ini"
-sleep 2
-echo $pur"SUBSCRIBE MY CHANNEL { BABANG HACK }"
-exit
-fi
-
