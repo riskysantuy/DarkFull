f56e48ef71a29d258ea28083e0a2e13e32f93f48
RiskySantuy <55824407+riskysantuy@users.noreply.github.com>
2019-10-01 19:16:08 +0700

    Create We-Anjing-Mau-Recode-Punya-Gue-Tak_Takut-lo-Sama-ALLAH-Udah_recode-Punya-Gue-Heeee_Tapi-TakTaku-Sama-Yang-Maha-Kuasa-Astafiruloh-Dasar-Tidak_Ada-Otak_Bst-Kau-Burung-Panjang-awdawdawdawdawdawdawdawdawdawdxawdxawdxawdxawdxawxdawxawdxawxaxawxawdxawdxawdxaxwdiajiwdxjiawjxdioajioxwjdioxajwiodxjawioxjaiwdoxjiaowxjdioawjxiodajwxj.sh

---
+++ We-Anjing-Mau-Recode-Punya-Gue-Tak_Takut-lo-Sama-ALLAH-Udah_recode-Punya-Gue-Heeee_Tapi-TakTaku-Sama-Yang-Maha-Kuasa-Astafiruloh-Dasar-Tidak_Ada-Otak_Bst-Kau-Burung-Panjang-awdawdawdawdawdawdawdawdawdawdxawdxawdxawdxawdxawxdawxawdxawxaxawxawdxawdxawdxaxwdiajiwdxjiawjxdioajioxwjdioxajwiodxjawioxjaiwdoxjiaowxjdioawjxiodajwxj.sh
@@ -0,0 +1,285 @@
+# Kumpulan Tools Work
+# Yang Bikin By Risky
+# Mau recode? Bangsat Kau Tak Ada Otak
+
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+
+clear
+# yellow= \033[33;1m
+# indigo= \033[34;1m
+# red= \033[31;1m
+# purple= \033[37;1m
+# cyan= \033[36;1m
+# white= \033[39;1m
+
+echo $ku"Tools Ini Work Dan Full"
+sleep 1
+echo $ku"Jika Ada Tools Kalian"
+sleep 5
+echo $i"Bilang Sama Admin Biar Gue Masukan KeTools Ini"
+sleep 5
+echo $i"Tapi Jangan Recode Bst Gue Susah MemBuatnya"
+sleep 10
+echo $me"Yang Rocode Tools Gue Saya Do'aKan Mamanya Mati!!!"
+sleep 10
+sleep 5
+echo $me"Amin'Amin Ya ALLAH Amin'Ya Tuhan'Amin'Ya Dewa"
+sleep 10
+sleep 5
+
+z() {
+echo "
+$i░░█░█▌█▀▀█ █▀▀█ █▀▀█ █▌▄█░░
+░░█▀█▌█▄▄█ █▄▄█ █▄▄█ █▄██░░
+░░█░█▌█▌▐█ █▌░░ █▌░░ ░██░░░
+░$ku╔╦═╦╗───╔╗──────╔╗─$me▄▄─▄▄▀▀▄▀▀▄
+░$ku║║║║╠═╦═╣╠╦═╦═╦╦╝║$me███████───▄▀
+ $ku║║║║║╩╣╩╣═╣╩╣║║║╬║$me▀█████▀▀▄▀
+░$ku╚═╩═╩═╩═╩╩╩═╩╩═╩═╝──$me▀█▀ ♥♥
+        $pur◕▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬◕
+             $pu █████████ 
+              █▄█████▄█ 
+              █$me ▼▼▼▼▼$pu █ 
+            $pu ██▌    $pu ██▌ 
+             $pu █$me ▲▲▲▲▲$pu █ 
+              █████████ 
+              ██     ██
+ $i▂▃▅▇█▓▒░۩۞۩$me╔══╗$cy╔═╗$i╔═╗$ku╔╗$bi─╔══╗$pur╔╗╔╗$i۩۞۩░▒▓█▇▅▃▂
+            $me╚╗╔╝$cy║║║$i║║║$ku║║$bi─║══╣$pur╚╗╔╝
+            $me ║║ $cy║║║$i║║║$ku║╚╗$bi╠══║$pur╔╝╚╗
+            $me ╚╝ $cy╚═╝$i╚═╝$ku╚═╝$bi╚══╝$pur╚╝╚╝
+"
+}
+z
+echo
+sleep 1
+echo $me"==================="$ku" ============================="
+sleep 1
+echo $i"Ôûê"$pu"  GitHub    :"$me" riskysantuy"$i"             Ôûê"
+sleep 1
+echo $i"Ôûê"$pu"  FACEBOOK  :"$cy" Hi Tumbal Face'boom Hi"$i"  Ôûê"
+sleep 1
+echo $i"Ôûê"$pu"  WhatsApp  :"$i" *************"$i"           Ôûê"
+sleep 1
+echo $i"Ôûê"$pu"  GMAIL     :"$ku" riskysantuy101@gmail.com"$i"Ôûê"
+sleep 1
+echo $i"Ôûê"$pu"  INSTAGRAM :"$bi" Tidak Ada Soryy"$i"         Ôûê"
+sleep 1
+echo $me"==================="$ku" ============================="
+echo
+sleep 1
+echo $ku"MeNu Tools Vvip"
+sleep 1
+sleep 2
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"01"$me"[]"$cy" DarkFB 1.3"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"02"$me"[]"$cy" TuanBadut"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"03"$me"[]"$cy" DarkCyber"
+echo $me"[]"$i"##"$me"[]"$cy" User:BOCIL Pass:N00B"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"04"$me"[]"$cy" MBF (Facebook Target)"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"05"$me"[]"$cy" Bajingan"
+echo $me"[]"$i"##"$me"[]"$cy" User:BAJINGAN Pass:Gans"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"06"$me"[]"$cy" Rusmana-ID"
+echo $me"[]"$i"##"$me"[]"$cy" Tidak Pake User & Pass"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"07"$me"[]"$cy" XtermuxRider"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"08"$me"[]"$cy" SPAM Whatshapp"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"09"$me"[]"$cy" SPAM SMS"         
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"10"$me"[]"$cy" Script-Deface"           
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"11"$me"[]"$cy" Spam Call"              
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"12"$me"[]"$cy" Santet-Online"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"99"$me"[]"$cy" Install Tools Full"
+echo $me"[]"$i"00"$me"[]"$cy" Exit Fack YOU!!"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+sleep 2
+sleep 5
+echo $pur"["$pur"Anjing Masukan Pilihan Anda"$pur"]"
+sleep 4
+read -p " Nomor Berapa Bst>>>>>>> " pil
+ 
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TheMagizz/DarkPremium
+cd DarkPremium
+python2 DarkFB.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+sh TUANB$DUT.sh
+fi
+ 
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/fatonicyber/Darkcyber
+cd Darkcyber
+sh Darkcyber.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+fi
+ 
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh install.sh
+fi
+ 
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/aldidefril556/XtermuxRider
+cd XtermuxRider
+sh canggih123.sh
+
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+fi
+ 
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+python2 run.py
+fi
+ 
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+python2 create.py
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Aditya021/SpamCall
+cd SpamCall
+php SpamCall.php
+fi
+ 
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+fi
+
+if [ $pil = 99 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pkg update
+clear
+pkg upgrade
+clear 
+pkg install git
+clear
+pkg install python1
+clear
+pkg install python2
+clear
+pkg install python3
+clear
+figlet -f slant "SELESAI"|lolcat
+sleep 1
+sleep 2
+sleep 3
+sh DarkFull.sh
+fi
+
+if [ $pil = 99 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 10
+echo $me"Apakah Anda Mau Keluar, Dari Tools Ini??"
+sleep 5
+echo $ku"Kolo Anda Ingin Keluar, Pant*kKau!!"
+sleep 10
+echo $cy"DasarKau Bajingan,Bangs*t,Anjin*,Bab*"
+sleep 2
+echo $i"Terima-Kasih""
+sleep 10
+sleep 2
+exit
+exit
+exit
