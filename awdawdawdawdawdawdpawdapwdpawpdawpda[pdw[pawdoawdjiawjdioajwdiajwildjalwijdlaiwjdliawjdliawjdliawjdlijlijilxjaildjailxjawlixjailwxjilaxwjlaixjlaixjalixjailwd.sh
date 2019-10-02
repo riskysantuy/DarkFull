a84250aac5cac384a15326656d1ef52f5a78c2bd
RiskySantuy <55824407+riskysantuy@users.noreply.github.com>
2019-10-02 16:58:25 +0700

    Update awdawdawdawdawdawdpawdapwdpawpdawpda[pdw[pawdoawdjiawjdioajwdiajwildjalwijdlaiwjdliawjdliawjdliawjdlijlijilxjaildjailxjawlixjailwxjilaxwjlaixjlaixjalixjailwd.sh

--- awdawdawdawdawdawdpawdapwdpawpdawpda[pdw[pawdoawdjiawjdioajwdiajwildjalwijdlaiwjdliawjdliawjdliawjdlijlijilxjaildjailxjawlixjailwxjilaxwjlaixjlaixjalixjailwd.sh
+++ awdawdawdawdawdawdpawdapwdpawpdawpda[pdw[pawdoawdjiawjdioajwdiajwildjalwijdlaiwjdliawjdliawjdliawjdlijlijilxjaildjailxjawlixjailwxjilaxwjlaixjlaixjalixjailwd.sh
@@ -1,2 +1,104 @@
+# Kumpulan Tools Work
+# Yang Bikin By Risky
+# Mau recode? Bangsat Kau Tak Ada Otak
+
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+
+clear
+# yellow= \033[33;1m
+# indigo= \033[34;1m
+# red= \033[31;1m
+# purple= \033[37;1m
+# cyan= \033[36;1m
+# white= \033[39;1m
+
+clear
+sleep 5
+
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+sleep 3
+echo $me"[]"$i"Kumpulan UserName & Password Dark Work"
+sleep 3
+echo $me"[]"$bi"Nama Admin Tools:Risky Solihin"
+sleep 3
+echo $me"[]"$ku"Nama Pacar      :Kepo Kali Kau Bst"
+sleep 3
+echo $me"[]"$me"Nama Orang Tua  :Kepo Kali Kau Bst"
+sleep 3
+echo $me"[]"$bi"Chanel Youtobe  :Babang Hack"
+sleep 3
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+sleep 2
+echo
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+sleep 2
+echo $me"[]"$i"01"$me"[]"$cy" Membuka Web UserName & Password"
+sleep 2
+echo $me"[]"$i"##"$me"[]"$cy" Hati-Hati Kenak Tipu"
+sleep 2
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+sleep 2
+echo $me"[]"$i"02"$me"[]"$cy" Download UserName & Password"
+sleep 2
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+sleep 2
+
+echo $pur"["$pur"Masukan Yang Harus Dipilih"$pur"]"
+sleep 4
+read -p " {Nomor Berapa Bst>>>>>>>} " pil
 
 
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 5
+figlet -f slant "Mampus"|lolcat
+sleep 2
+figlet -f slant "Kenak Tipu"|lolcat
+sleep 2
+figlet -f slant "Sama Admin"|lolcat
+sleep 4
+figlet -f slant "Makanya"|lolcat
+sleep 2
+figlet -f slant "Download"|lolcat
+sleep 10
+sleep 1
+sh awdawdawdawdawdawdpawdapwdpawpdawpda[pdw[pawdoawdjiawjdioajwdiajwildjalwijdlaiwjdliawjdliawjdliawjdlijlijilxjaildjailxjawlixjailwxjilaxwjlaixjlaixjalixjailwd.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 5
+figlet -f slant "Selamat"|lolcat
+sleep 4
+figlet -f slant "Anda"|lolcat
+sleep 4
+figlet -f slant "Tidak"|lolcat
+sleep 3
+figlet -f slant "Kenak"|lolcat
+sleep 4
+figlet -f slant "Tipu Sama"|lolcat
+sleep 5
+figlet -f slant "Admin!!"|lolcat
+sleep 5
+xdg-open https://bercara.com/BangsatKau999
+fi
