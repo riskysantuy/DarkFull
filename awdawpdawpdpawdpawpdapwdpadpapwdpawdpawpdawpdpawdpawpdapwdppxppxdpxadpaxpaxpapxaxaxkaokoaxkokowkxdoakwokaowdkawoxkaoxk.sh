911ab5f709947ec7699e40fbe91ae21a81785d20
RiskySantuy <55824407+riskysantuy@users.noreply.github.com>
2019-10-03 15:12:31 +0700

    Create awdawpdawpdpawdpawpdapwdpadpapwdpawdpawpdawpdpawdpawpdapwdppxppxdpxadpaxpaxpapxaxaxkaokoaxkokowkxdoakwokaowdkawoxkaoxk.sh

---
+++ awdawpdawpdpawdpawpdapwdpadpapwdpawdpawpdawpdpawdpawpdapwdppxppxdpxadpaxpaxpapxaxaxkaokoaxkokowkxdoakwokaowdkawoxkaoxk.sh
@@ -0,0 +1,180 @@
+# Jangan DiRecode Iya Tod
+
+clear
+figlet "W A I T"  | lolcat
+pkg install nano
+clear
+pkg install ruby cowsay toilet figlet
+clear
+pkg install neofetch
+clear
+gem install lolcat
+clear
+sleep 2
+
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+
+clear
+# yellow= \033[33;1m
+# indigo= \033[34;1m
+# red= \033[31;1m
+# purple= \033[37;1m
+# cyan= \033[36;1m
+# white= \033[39;1m
+
+clear
+
+echo $ku"Sebelum Masuk KeTools Ini Kita Harus Tonton Gambar Yang Ada"
+sleep 2
+echo $ku"TerimaKasih"
+sleep 8
+clear
+cowsay -f eyes "Welcome To Temux Indonesia & Salam Hacker" | lolcat
+figlet "Bangsat"  | lolcat
+figlet -f slant "Bangsat"|lolcat
+neofetch
+date | lolcat
+sleep 8
+clear
+echo $i"Gue Akan Mengasih Tau User & Pass Tools"
+neofetch | lolcat
+echo " Time : " $TIME | lolcat
+date | lolcat
+echo Username : "Anjing" | lolcat
+echo Hostname : @"Lepas" | lolcat
+figlet "RiskyGx" | lolcat
+sleep 5
+echo
+echo $me"                     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶"
+echo $me"                 ¶¶¶¶¶¶             ¶¶¶¶¶¶¶"
+echo $me"              ¶¶¶¶                        ¶¶¶¶"
+echo $me"             ¶¶¶                            ¶¶¶"
+echo $me"            ¶¶                                ¶¶"
+echo $me"           ¶¶                                  ¶¶"
+echo $me"          ¶¶                                   ¶¶"
+echo $me"          ¶¶´¶¶                             ¶¶´¶¶"
+echo $me"          ¶¶´¶¶                             ¶¶´´¶"
+echo $me"          ¶¶´¶¶                             ¶¶´´¶"
+echo $me"         ¶¶  ¶¶                            ¶¶´¶¶"
+echo $me"          ¶¶´´¶¶                           ¶¶´´¶¶"
+echo $me"           ¶¶´¶¶   ¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶   ¶¶´¶¶"
+echo $me"            ¶¶¶¶´¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶¶¶"
+echo $me"             ¶¶¶´¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶ ¶¶¶"
+echo $me"    ¶¶¶       ¶¶  ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶¶¶  ¶¶      ¶¶¶¶"
+echo $me"   ¶¶¶¶¶     ¶¶   ¶¶¶¶¶¶¶   ¶¶¶   ¶¶¶¶¶¶¶   ¶¶     ¶¶¶¶¶¶"
+echo $me"  ¶¶   ¶¶    ¶¶     ¶¶¶    ¶¶¶¶¶    ¶¶¶     ¶¶    ¶¶   ¶¶"
+echo $me" ¶¶¶    ¶¶¶¶  ¶¶          ¶¶¶¶¶¶¶          ¶¶  ¶¶¶¶    ¶¶¶"
+echo $me"¶¶         ¶¶¶¶¶¶¶¶       ¶¶¶¶¶¶¶        ¶¶¶¶¶¶¶¶¶        ¶¶"
+echo $me"¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶    ¶¶¶¶¶¶¶¶      ¶¶¶¶¶¶¶¶"
+echo $me"  ¶¶¶¶ ¶¶¶¶¶      ¶¶¶¶¶              ¶¶¶ ¶¶     ¶¶¶¶¶¶ ¶¶¶"
+echo $me"       ¶¶¶¶¶¶  ¶¶¶  ¶¶           ¶¶  ¶¶¶  ¶¶¶¶¶¶"        
+echo $me"              ¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶¶¶¶¶¶ ¶¶ ¶¶¶¶¶¶"
+echo $me"                  ¶¶ ¶¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶¶"
+echo $me"                ¶¶¶¶  ¶ ¶ ¶ ¶ ¶ ¶ ¶ ¶   ¶¶¶¶¶"
+echo $me"            ¶¶¶¶¶ ¶¶   ¶¶¶¶¶¶¶¶¶¶¶¶¶   ¶¶ ¶¶¶¶¶"
+echo $me"    ¶¶¶¶¶¶¶¶¶¶     ¶¶                 ¶¶      ¶¶¶¶¶¶¶¶¶"
+echo $me"   ¶¶           ¶¶¶¶¶¶¶             ¶¶¶¶¶¶¶¶          ¶¶"
+echo $me"    ¶¶¶     ¶¶¶¶¶     ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶     ¶¶¶¶¶     ¶¶¶"
+echo $me"      ¶¶   ¶¶¶           ¶¶¶¶¶¶¶¶¶           ¶¶¶   ¶¶"
+echo $me"      ¶¶  ¶¶                                   ¶¶  ¶¶"
+echo $me"       ¶¶¶¶                                     ¶¶¶¶"
+echo
+echo
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo $i  "AUTHOR   : Mr.RiskyGx"
+echo $i  "WA       : 0813651823**"
+echo $i  "YOUTUBE  : Babang Hack"
+echo $i  "TOOLS    : Tools Full"
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo
+echo
+z() {
+echo "
+$i░░█░█▌█▀▀█ █▀▀█ █▀▀█ █▌▄█░░
+░░█▀█▌█▄▄█ █▄▄█ █▄▄█ █▄██░░
+░░█░█▌█▌▐█ █▌░░ █▌░░ ░██░░░
+░$ku╔╦═╦╗───╔╗──────╔╗─$me▄▄─▄▄▀▀▄▀▀▄
+░$ku║║║║╠═╦═╣╠╦═╦═╦╦╝║$me███████───▄▀
+ $ku║║║║║╩╣╩╣═╣╩╣║║║╬║$me▀█████▀▀▄▀
+░$ku╚═╩═╩═╩═╩╩╩═╩╩═╩═╝──$me▀█▀ ♥♥
+        $pur◕▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬◕
+             $pu █████████ 
+              █▄█████▄█ 
+              █$me ▼▼▼▼▼$pu █ 
+            $pu ██▌    $pu ██▌ 
+             $pu █$me ▲▲▲▲▲$pu █ 
+              █████████ 
+              ██     ██
+ $i▂▃▅▇█▓▒░۩۞۩$me╔══╗$cy╔═╗$i╔═╗$ku╔╗$bi─╔══╗$pur╔╗╔╗$i۩۞۩░▒▓█▇▅▃▂
+            $me╚╗╔╝$cy║║║$i║║║$ku║║$bi─║══╣$pur╚╗╔╝
+            $me ║║ $cy║║║$i║║║$ku║╚╗$bi╠══║$pur╔╝╚╗
+            $me ╚╝ $cy╚═╝$i╚═╝$ku╚═╝$bi╚══╝$pur╚╝╚╝
+"
+}
+z
+echo
+sleep 1
+echo $me"==================="$ku" ============================="
+sleep 1
+echo $i"Ôûê"$pu"  GitHub    :"$me" riskysantuy"$i"             Ôûê"
+sleep 1
+echo $i"Ôûê"$pu"  FACEBOOK  :"$cy" Hi Tumbal Face'boom Hi"$i"  Ôûê"
+sleep 1
+echo $i"Ôûê"$pu"  WhatsApp  :"$i" *************"$i"           Ôûê"
+sleep 1
+echo $i"Ôûê"$pu"  GMAIL     :"$ku" riskysantuy101@gmail.com"$i"Ôûê"
+sleep 1
+echo $i"Ôûê"$pu"  INSTAGRAM :"$bi" Tidak Ada Soryy"$i"         Ôûê"
+sleep 1
+echo $me"==================="$ku" ============================="
+echo
+sleep 1
+echo $ku"MeNu Tools Vvip"
+sleep 1
+sleep 2
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"01"$me"[]"$cy" Adi1090x"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+echo $me"[]"$i"02"$me"[]"$cy" Remo773"
+echo $me"[][][][][][][][][][][][][][][][][][][][][][][][][][]"
+sleep 5
+echo $pur"["$pur"Bangsat Masukan Pilihan"$pur"]"
+sleep 4
+read -p " =========>>>>> " pil
+ 
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 2
+git clone https://github.com/adi1090x/termux-style.git
+cd termux-style
+chmod +x setup
+./setup 
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 2
+git clone https://github.com/remo7777/T-Header.git
+cd T-Header
+bash t-header.sh 
+fi
